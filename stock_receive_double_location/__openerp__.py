c55352c75fb0a6d4734908024e3f80e1cdb6c07e
David Beal <david.beal@akretion.com>
2017-06-13 22:36:23 +0200

    ADD stock_receive_double_location

---
+++ stock_receive_double_location/__openerp__.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+# © 2017 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Receive Double Location",
+    "summary": "Stock Receive Double Location",
+    "version": "8.0.1.0.0",
+    "category": "Warehouse",
+    "website": "https://odoo-community.org/",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        # "views/res_partner_view.xml",
+    ],
+    "demo": [
+        # "demo/res_partner_demo.xml",
+    ],
+}
---
+++ stock_receive_double_location/models/stock_move.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# © 2017 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    # picking_location_id = fields.Many2one(
+    #     comodel_name='stock.location', string="Picking Location",
+    #     help="In overstock locations, you can define the place where "
+    #          "picking products are available.")
