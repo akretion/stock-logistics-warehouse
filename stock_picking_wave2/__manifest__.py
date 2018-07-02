d5ec4c7cec2eee7941167f9cafdc62a9ac8df582
Pierrick Brun <brun.pierrick@protonmail.com>
2018-07-02 16:41:20 +0200

    [ADD] port stock_picking_batch from 11.0 (as wave2)

---
+++ stock_picking_wave2/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Warehouse Management: Wave Picking',
+    'version': '1.0',
+    'category': 'Warehouse',
+    'description': """
+This module adds the wave picking option in warehouse management
+=================================================================
+    """,
+    'website': 'https://www.odoo.com/page/warehouse',
+    'depends': ['stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/stock_picking_wave_views.xml',
+        'data/stock_picking_wave_data.xml',
+        'wizard/stock_picking_to_wave_views.xml',
+    ],
+    'demo': [
+        'data/stock_picking_wave_demo.xml',
+    ],
+    'installable': True,
+}
