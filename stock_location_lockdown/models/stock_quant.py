788125b921bcd350a083326de564f988e0fc594b
Sébastien BEAU <sebastien.beau@akretion.com>
2019-07-05 19:32:12 +0200

    [FIX] fix negative quant in blocked location
    When forcing an outgoing move and then moving it. Odoo create a
    negative quant. This quant should not be in the locked location
    because no quand will go in it and so the negative quant will stay
    here for ever

--- stock_location_lockdown/models/stock_quant.py
+++ stock_location_lockdown/models/stock_quant.py
@@ -2,17 +2,20 @@
 # Copyright 2018 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, exceptions, models, _
+from openerp import api, models, _
+from openerp.exceptions import ValidationError
 
 
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    @api.model
-    def _check_location(self, location):
-        res = super(StockQuant, self)._check_location(location)
-        if location.block_stock_entrance:
-            raise exceptions.Warning(
-                _('The location %s is not configured to receive stock.')
-                % (location.name))
-        return res
+    @api.constrains('location_id')
+    def _check_location_blocked(self):
+        for record in self:
+            if record.location_id.block_stock_entrance:
+                raise ValidationError(
+                    _('The location %s is blocked and can '
+                      'not be used for moving the product %s')
+                    % (record.location_id.name, record.product_id.name)
+                    )
+        return True
