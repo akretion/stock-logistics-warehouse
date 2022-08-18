69cbdf64ac13383f8d9c9b1c93d1c99514dd42c2
Florian da Costa <florian.dacosta@akretion.com>
2022-08-18 17:23:35 +0200

    [FIX] warehouse_id on location if the parent of the parent location is set afterward. Actually, this does not fix anything in v8, depending on computed field does not seem to work, but hopefully, it will be usefull at migration

--- stock_multi_warehouse_security/models/stock_location.py
+++ stock_multi_warehouse_security/models/stock_location.py
@@ -9,7 +9,10 @@ class StockLocation(models.Model):
     _inherit = "stock.location"
 
     @api.multi
-    @api.depends('location_id')
+    # FIXME this depends on warehouse_id does not work in v8.
+    # I put it to solve this on migration, but only location_id is taken into account
+    # which can lead to some bug on warehouse computation...
+    @api.depends('location_id.warehouse_id')
     def _compute_warehouse(self):
         for location in self:
             location.warehouse_id = self.get_warehouse(location)
